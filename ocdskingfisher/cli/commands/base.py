8cfe21df55e281c5f1f61c7bd090741bac20186f
James Baster <james.baster@opendataservices.coop>
2018-09-06 15:24:40 +0100

    Move common load code to base

--- ocdskingfisher/cli/commands/base.py
+++ ocdskingfisher/cli/commands/base.py
@@ -1,3 +1,4 @@
+import ocdskingfisher.sources_util
 
 
 class CLICommand:
@@ -11,3 +12,26 @@ class CLICommand:
 
     def run_command(self, args):
         pass
+
+    def configure_subparser_for_selecting_existing_collection(self, subparser):
+        subparser.add_argument("--run", help="source")
+        subparser.add_argument("--dataversion", help="Specify a data version")
+        subparser.add_argument("--sample", help="Sample source only", action="store_true")
+
+    def run_command_for_selecting_existing_collection(self, args):
+
+        sources = ocdskingfisher.sources_util.gather_sources()
+
+        sample_mode = args.sample
+        data_version = args.dataversion
+
+        if args.run not in sources:
+            print("We can not find a source that you requested!")
+            quit(-1)
+
+        self.collection_instance = sources[args.run](self.app.data_dir,
+                                                          remove_dir=False,
+                                                          sample=sample_mode,
+                                                          data_version=data_version,
+                                                          new_version=False
+                                                          )
--- ocdskingfisher/cli/commands/check_different_schema_version.py
+++ ocdskingfisher/cli/commands/check_different_schema_version.py
@@ -1,41 +1,22 @@
 import ocdskingfisher.database
 import ocdskingfisher.cli.commands.base
-import ocdskingfisher.sources_util
 
 
 class CheckDifferentSchemaVersionCLICommand(ocdskingfisher.cli.commands.base.CLICommand):
     command = 'check-different-schema-version'
 
-    def __init__(self, app=None):
-        self.app = app
-        self.sources = ocdskingfisher.sources_util.gather_sources()
-
     def configure_subparser(self, subparser):
-        subparser.add_argument("--run", help="source")
-        subparser.add_argument("--dataversion", help="Specify a data version")
-        subparser.add_argument("--sample", help="Sample source only", action="store_true")
+        self.configure_subparser_for_selecting_existing_collection(subparser)
         subparser.add_argument("--schemaversion", help="Set Schema Version - defaults to 1.1")
 
     def run_command(self, args):
 
-        sample_mode = args.sample
-        data_version = args.dataversion
-        override_schema_version = args.schemaversion or "1.1"
+        self.run_command_for_selecting_existing_collection(args)
 
+        override_schema_version = args.schemaversion or "1.1"
         schema_versions = ["1.0", "1.1"]
         if override_schema_version not in schema_versions:
             print("We do not recognise that schema version! Options are: %s" % ", ".join(schema_versions))
             quit(-1)
 
-        if args.run not in self.sources:
-            print("We can not find a source that you requested!")
-            quit(-1)
-
-        instance = self.sources[args.run](self.app.data_dir,
-                                          remove_dir=False,
-                                          sample=sample_mode,
-                                          data_version=data_version,
-                                          new_version=False
-                                          )
-
-        instance.run_check(override_schema_version)
+        self.collection_instance.run_check(override_schema_version)
